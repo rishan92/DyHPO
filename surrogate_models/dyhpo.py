0957a78747a9abad6543c16fad965f8fedf9db68
Rishan <rd.senanayake@gmail.com>
2023-05-24 16:02:31 +0200

    Add power law feature net

--- surrogate_models/dyhpo.py
+++ surrogate_models/dyhpo.py
@@ -12,6 +12,123 @@ from torch import cat
 import gpytorch
 
 
+class FeatureExtractorPowerLaw(nn.Module):
+    """
+    The feature extractor that is part of the deep kernel.
+    """
+    def __init__(self, configuration):
+        super().__init__()
+
+        self.configuration = configuration
+
+        self.nr_layers = configuration['nr_layers']
+        self.act_func = nn.LeakyReLU()
+        self.last_act_func = nn.GELU()
+        # adding one to the dimensionality of the initial input features
+        # for the concatenation with the budget.
+        initial_features = configuration['nr_initial_features']  # + 1
+        self.fc1 = nn.Linear(initial_features, configuration['layer1_units'])
+        self.bn1 = nn.BatchNorm1d(configuration['layer1_units'])
+        for i in range(2, self.nr_layers):
+            setattr(
+                self,
+                f'fc{i + 1}',
+                nn.Linear(configuration[f'layer{i - 1}_units'], configuration[f'layer{i}_units']),
+            )
+            setattr(
+                self,
+                f'bn{i + 1}',
+                nn.BatchNorm1d(configuration[f'layer{i}_units']),
+            )
+
+
+        setattr(
+            self,
+            f'fc{self.nr_layers}',
+            nn.Linear(
+                configuration[f'layer{self.nr_layers - 1}_units'],
+                # configuration['cnn_nr_channels'],  # accounting for the learning curve features
+                configuration[f'layer{self.nr_layers}_units']
+            ),
+        )
+        setattr(
+            self,
+            f'fc{self.nr_layers + 1}',
+            nn.Linear(configuration[f'layer{self.nr_layers}_units'], 3),
+        )
+        self.cnn = nn.Sequential(
+            nn.Conv1d(in_channels=1, kernel_size=(configuration['cnn_kernel_size'],), out_channels=4),
+            nn.AdaptiveMaxPool1d(1),
+        )
+
+    def forward(self, x, budgets, learning_curves):
+
+        # add an extra dimensionality for the budget
+        # making it nr_rows x 1.
+
+        # concatenate budgets with examples
+        # x = cat((x, budgets), dim=1)
+        x = self.fc1(x)
+        x = self.act_func(self.bn1(x))
+
+        for i in range(2, self.nr_layers):
+            x = self.act_func(
+                getattr(self, f'bn{i}')(
+                    getattr(self, f'fc{i}')(
+                        x
+                    )
+                )
+            )
+
+        # add an extra dimensionality for the learning curve
+        # making it nr_rows x 1 x lc_values.
+        # learning_curves = torch.unsqueeze(learning_curves, 1)
+        # lc_features = self.cnn(learning_curves)
+        # # revert the output from the cnn into nr_rows x nr_kernels.
+        # lc_features = torch.squeeze(lc_features, 2)
+
+        # put learning curve features into the last layer along with the higher level features.
+        # x = cat((x, lc_features), dim=1)
+        x = self.act_func(getattr(self, f'fc{self.nr_layers}')(x))
+
+        x = getattr(self, f'fc{self.nr_layers+1}')(x)
+        # x = cat((x, budgets), dim=1)
+
+        alphas = x[:, 0]
+        betas = x[:, 1]
+        gammas = x[:, 2]
+        betas = self.last_act_func(betas)
+        gammas = self.last_act_func(gammas)
+
+        output = torch.add(
+            alphas,
+            torch.mul(
+                betas,  # torch.mul(betas, -1),
+                torch.pow(
+                    budgets,
+                    torch.mul(gammas, -1)
+                )
+            ),
+        )
+
+        # info = {
+        #     'alpha': alphas,
+        #     'beta': betas,
+        #     'gamma': gammas,
+        #     'pl_output': output,
+        # }
+
+        budgets = torch.unsqueeze(budgets, dim=1)
+        alphas = torch.unsqueeze(alphas, dim=1)
+        betas = torch.unsqueeze(betas, dim=1)
+        gammas = torch.unsqueeze(gammas, dim=1)
+        output = torch.unsqueeze(output, dim=1)
+
+        x = cat((alphas, betas, gammas, output), dim=1)
+
+        return x
+
+
 class FeatureExtractor(nn.Module):
     """
     The feature extractor that is part of the deep kernel.
@@ -150,7 +267,10 @@ class DyHPO:
         check_seed_torch = torch.random.get_rng_state().sum()
         check_seed_np = np.sum(np.random.get_state()[1])
         check_seed_random = np.sum(random.getstate()[1])
-        self.feature_extractor = FeatureExtractor(configuration)
+        self.feature_net_class = FeatureExtractorPowerLaw
+        self.feature_extractor = self.feature_net_class(configuration)
+        self.feature_net_output_size = configuration[f'layer{self.feature_extractor.nr_layers}_units']
+        self.feature_net_output_size = 4
         self.batch_size = configuration['batch_size']
         self.nr_epochs = configuration['nr_epochs']
         self.early_stopping_patience = configuration['nr_patience_epochs']
@@ -159,7 +279,7 @@ class DyHPO:
         self.seed = seed
         self.model, self.likelihood, self.mll = \
             self.get_model_likelihood_mll(
-                configuration[f'layer{self.feature_extractor.nr_layers}_units']
+                self.feature_net_output_size
             )
 
         self.model.to(self.dev)
@@ -207,10 +327,10 @@ class DyHPO:
         check_seed_torch = torch.random.get_rng_state().sum()
         check_seed_np = np.sum(np.random.get_state()[1])
         check_seed_random = np.sum(random.getstate()[1])
-        self.feature_extractor = FeatureExtractor(self.configuration).to(self.dev)
+        self.feature_extractor = self.feature_net_class(self.configuration).to(self.dev)
         self.model, self.likelihood, self.mll = \
             self.get_model_likelihood_mll(
-                self.configuration[f'layer{self.feature_extractor.nr_layers}_units'],
+                self.feature_net_output_size
             )
 
         self.optimizer = torch.optim.Adam([
@@ -301,6 +421,7 @@ class DyHPO:
         mse = 0.0
 
         for epoch_nr in range(0, nr_epochs):
+            check_seed_torch = torch.random.get_rng_state().sum()
             nr_examples_batch = X_train.size(dim=0)
             # if only one example in the batch, skip the batch.
             # Otherwise, the code will fail because of batchnorm
